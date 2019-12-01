e6f79cf6fd47b4a80f337541443716156d9e8c69
IOEvan <43669309+IOEvan@users.noreply.github.com>
2019-12-01 11:17:47 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,52 +1,36 @@
-# kdd99数据集预处理
-# 将kdd99符号型数据转化为数值型数据
-
-# coding:utf-8
-
 import numpy as np
 import pandas as pd
 import csv
 import time
 
-global label_list  # label_list为全局变量
-
-
-# 定义kdd99数据预处理函数
 def preHandel_data():
     source_file = './data/1-kddcup.data_10_percent_corrected'
     # source_file = '3-corrected.txt'
     handled_file = './data/three_kddcup.data_10_percent_corrected.csv'
-    data_file = open(handled_file, 'w', newline='')  # python3.x中添加newline=''这一参数使写入的文件没有多余的空行
+    data_file = open(handled_file, 'w', newline='') 
     with open(source_file, 'r') as data_source:
         csv_reader = csv.reader(data_source)
         csv_writer = csv.writer(data_file)
         count = 0  # 记录数据的行数，初始化为0
         for row in csv_reader:
-            temp_line = np.array(row)  # 将每行数据存入temp_line数组里
-            temp_line[1] = handleProtocol(row)  # 将源文件行中3种协议类型转换成数字标识
-            temp_line[2] = handleService(row)  # 将源文件行中70种网络服务类型转换成数字标识
-            temp_line[3] = handleFlag(row)  # 将源文件行中11种网络连接状态转换成数字标识
-            temp_line[41] = handleLabel(row)  # 将源文件行中23种攻击类型转换成数字标识
-            # csv_writer.writerow(temp_line)
+            temp_line = np.array(row)  
+            temp_line[1] = handleProtocol(row)  
+            temp_line[2] = handleService(row) 
+            temp_line[3] = handleFlag(row) 
+            temp_line[41] = handleLabel(row)  
+            csv_writer.writerow(temp_line)
             count += 1
-            # 输出每行数据中所修改后的状态
+            
             print(count, 'status:', temp_line[1], temp_line[2], temp_line[3], temp_line[41])
         data_file.close()
 
-
-# 将相应的非数字类型转换为数字标识即符号型数据转化为数值型数据
-def find_index(x, y):
-    return [i for i in range(len(y)) if y[i] == x]
-
-
-# 定义将源文件行中3种协议类型转换成数字标识的函数
+# 3种协议类型转换成数字
 def handleProtocol(input):
     protocol_list = ['tcp', 'udp', 'icmp']
     if input[1] in protocol_list:
         return protocol_list.index(input[1])
 
-
-# 定义将源文件行中70种网络服务类型转换成数字标识的函数
+# 71种网络服务类型转换成数字标识
 def handleService(input):
     service_list = ['aol', 'auth', 'bgp', 'courier', 'csnet_ns', 'ctf', 'daytime', 'discard', 'domain', 'domain_u',
                     'echo', 'eco_i', 'ecr_i', 'efs', 'exec', 'finger', 'ftp', 'ftp_data', 'gopher', 'harvest',
@@ -66,7 +50,7 @@ def handleService(input):
         print("2", input)
 
 
-# 定义将源文件行中11种网络连接状态转换成数字标识的函数
+# 11种网络连接状态转换成数字
 def handleFlag(input):
     flag_list = ['OTH', 'REJ', 'RSTO', 'RSTOS0', 'RSTR', 'S0', 'S1', 'S2', 'S3', 'SF', 'SH']
     if input[3] in flag_list:
@@ -75,7 +59,7 @@ def handleFlag(input):
         print("3", input[3])
 
 
-# 定义将源文件行中攻击类型转换成数字标识的函数(训练集中共出现了22个攻击类型，而剩下的17种只在测试集中出现)
+# 攻击类型转换成数字
 def handleLabel(input):
     label_list=['normal.', 'buffer_overflow.', 'loadmodule.', 'perl.', 'neptune.', 'smurf.',
     'guess_passwd.', 'pod.', 'teardrop.', 'portsweep.', 'ipsweep.', 'land.', 'ftp_write.',
@@ -94,4 +78,4 @@ if __name__ == '__main__':
     label_list = []
     preHandel_data()
     end_time = time.clock()
-    print("Running time:", (end_time - start_time))  # 输出程序运行时间
+    print("Running time:", (end_time - start_time))
